5039ed781c140a6b46b51281cc6e4d2391681e4d
Harsh Patil <harsh3152002@gmail.com>
2025-03-02 19:22:24 +0530

    Learnt how to setup react and and created a basic webpage

---
+++ firstReactApp/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Coffee from './Coffee';
+
+
+function App() {
+
+  return (
+    <Coffee/>
+  );
+}
+
+export default App
---
+++ firstReactApp/src/Coffee.jsx
@@ -0,0 +1,7 @@
+function Coffee(){
+    return (
+        <h3>We have prepared our coffee</h3>
+    )
+}
+
+export default Coffee
